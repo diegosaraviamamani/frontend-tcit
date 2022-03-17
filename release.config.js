b4f1c5dccc273e8b562ad5b581d81e46266d1e69
Diego Saravia Mamani <63614444+diegosaraviamamani@users.noreply.github.com>
2022-03-16 23:25:47 -0400

    reafactor: refactor

--- release.config.js
+++ release.config.js
@@ -14,6 +14,10 @@ module.exports = {
             type: 'docs',
             release: 'patch',
           },
+          {
+            type: 'refactor',
+            release: 'patch',
+          },
         ]
       },
     ],

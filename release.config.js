b79ab6783728c4ea4c04e92965d4963e71b53cdc
Diego Saravia Mamani <63614444+diegosaraviamamani@users.noreply.github.com>
2022-03-16 23:09:35 -0400

    style: some white-spaces

---
+++ release.config.js
@@ -0,0 +1,40 @@
+//semantic-release export config object
+module.exports = {
+  branches: [
+    'main',
+    'develop',
+  ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+        changelogTitle: '# Changelog',
+        changelogGeneratorOpts: {
+          preset: 'conventionalcommits',
+        },
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        "assets": [
+          "CHANGELOG.md",
+          "package.json"
+        ]
+      }
+    ],
+  ],
+}
